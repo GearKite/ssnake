fc1bb4cc59c1bc9e87f498f6b223f13a8215f7c7
GearKite <em@em.id.lv>
2024-03-16 12:59:17 +0200

    Improve main menu

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -3,67 +3,43 @@ import { GameObjects, Scene } from "phaser";
 import { EventBus } from "../EventBus";
 
 export class MainMenu extends Scene {
-  background: GameObjects.Image;
   logo: GameObjects.Image;
   title: GameObjects.Text;
-  logoTween: Phaser.Tweens.Tween | null;
+  playButton: GameObjects.Text;
 
   constructor() {
     super("MainMenu");
   }
 
   create() {
-    this.background = this.add.image(512, 384, "background");
+    this.cameras.main.setBackgroundColor(0x6b7aa1);
 
-    this.logo = this.add.image(512, 300, "logo").setDepth(100);
+    this.logo = this.add.image(512, 150, "logo").setDepth(100);
 
-    this.title = this.add
-      .text(512, 460, "Main Menu", {
+    this.playButton = this.add
+      .text(512, 460, "Play", {
         fontFamily: "Arial Black",
         fontSize: 38,
         color: "#ffffff",
         stroke: "#000000",
-        strokeThickness: 8,
+        strokeThickness: 3,
         align: "center",
+        backgroundColor: "#FFF767",
+        padding: { x: 8, y: 8 },
       })
       .setOrigin(0.5)
-      .setDepth(100);
+      .setDepth(100)
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.scene.start("Game");
+      })
+      .on("pointerover", () => {
+        this.playButton.setStyle({ backgroundColor: "#FFFFBF" });
+      })
+      .on("pointerout", () => {
+        this.playButton.setStyle({ backgroundColor: "#FFF767" });
+      });
 
     EventBus.emit("current-scene-ready", this);
-  }
-
-  changeScene() {
-    if (this.logoTween) {
-      this.logoTween.stop();
-      this.logoTween = null;
-    }
-
-    this.scene.start("Game");
-  }
-
-  moveLogo(vueCallback: ({ x, y }: { x: number; y: number }) => void) {
-    if (this.logoTween) {
-      if (this.logoTween.isPlaying()) {
-        this.logoTween.pause();
-      } else {
-        this.logoTween.play();
-      }
-    } else {
-      this.logoTween = this.tweens.add({
-        targets: this.logo,
-        x: { value: 750, duration: 3000, ease: "Back.easeInOut" },
-        y: { value: 80, duration: 1500, ease: "Sine.easeOut" },
-        yoyo: true,
-        repeat: -1,
-        onUpdate: () => {
-          if (vueCallback) {
-            vueCallback({
-              x: Math.floor(this.logo.x),
-              y: Math.floor(this.logo.y),
-            });
-          }
-        },
-      });
-    }
   }
 }
