be1ef08cd9d83792ed95d0cc1e8277c56a5b88dc
GearKite <em@em.id.lv>
2024-03-19 14:26:46 +0200

    Fix CSP error

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,14 @@ const http = createServer(app);
 
 new Game(http);
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
+  );
+  next();
+});
+
 app.use(express.static("public"));
 
 http.listen(port, () => {
